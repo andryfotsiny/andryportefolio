36b2bbe46e4ce085a222f7d6fa8af33c63b68d44
Andry <andryfotsiny1410@gmail.com>
2024-08-04 16:40:17 +0300

    Mise à jour de package.json et he

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -23,7 +23,7 @@ const Header: React.FC = () => {
                 </div>
                 <button onClick={() => setDarkMode(!darkMode)} className="ml-20 px-3 py-2 border-2 border-gray-700 dark:border-gray-200 rounded-lg font-bold text-gray-700 dark:text-gray-200 transition-colors duration-300"
                 >
-                    {darkMode ? <FaSun className="  " /> : <FaMoon className="" />}
+                    {darkMode ? <FaSun className=" " /> : <FaMoon className="" />}
                 </button>
 
                 <div className="md:hidden items-end">
